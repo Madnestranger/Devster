cb8c30b36b7977ec91d01cdbb077b1bed1098ec8
Anton Belous <woodgate@i.ua>
2017-03-06 21:32:18 +0200

    changes : 06-03-17

--- src/app/template/template.ctrl.js
+++ src/app/template/template.ctrl.js
@@ -3,7 +3,7 @@ export class TemplateController {
     'ngInject';
     this.$scope = $scope;
     $('[data-toggle="tooltip"]').tooltip();
-    $scope.currentPage = 'appearance';
+    $scope.currentPage = 'behavior';
     this.offerOn = false;
     this.leftBlock = false;
     this.fullwidth = false;
@@ -12,6 +12,57 @@ export class TemplateController {
     this.leftBarVisible = false;
     this.rightPositionBar = false;
     this.needRight = false;
+
+
+    // User toggle button
+
+    $('.kl-user-btn').on('click', function (e) {
+      e.preventDefault();
+      $(this).toggleClass('active');
+      $('.kl-user-list').slideToggle();
+    });
+
+
+    // Url buttons (BEHAVIOR & TARGETING RULES)
+
+    $('body')
+
+      .on('click', '.kl-settings-title', function () {
+
+        $(this).toggleClass('closed')
+          .siblings('.kl-settings-content').stop(true).slideToggle();
+
+      })
+
+      .on('click', '.kl-settings-content-block__add', function () {
+
+        $('.kl-settings-content-url-block').stop(true).slideToggle();
+
+      })
+
+      .on('click', '.kl-settings-drop-block button', function () {
+        $(this)
+          .toggleClass('active')
+            .siblings('.kl-settings-drop-down-block')
+              .stop(true).slideToggle();
+      })
+
+      .on('click', '.kl-settings-drop-down-block label', function () {
+        $(this).parents('.kl-settings-drop-block')
+          .find('.kl-settings-drop-input').val($(this).find('input').val())
+            .siblings('.kl-settings-drop-down-block').stop(true).slideUp()
+              .siblings('.kl-settings-drop-button').removeClass('active')
+                .find('.kl-settings-drop-button-name').text($(this).find('span').text());
+      })
+
+      .on('click', '.kl-settings-content-label', function () {
+        $(this).toggleClass('active')
+          .siblings('.kl-settings-content-label-block').stop(true).slideToggle();
+      })
+
+
+
+
   }
 
   setContent(page) {
